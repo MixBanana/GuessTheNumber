1e7c51de18dbb5a2c340cd89375902ed82ba7175
Sahar Shlichove <49879157+MixBanana@users.noreply.github.com>
2020-10-25 11:46:41 +0200

    Update Client.py
    Remove counter from client side

--- Client.py
+++ Client.py
@@ -3,18 +3,15 @@
 import socket
 
 running = 1
-counter = 0
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect(("127.0.0.1",1337))
 
 while running:
     reply = s.recv(2048).decode()
     print(reply)
-    counter += 1
-    if (counter==6): # Client side counter check
-        #print(F"Client guess counter: {counter}") # DEBUG COUNTER
+    if("Good job" in reply):
         quit()
-    if("Good job!" in reply):
+    elif("No more" in reply):
         quit()
     data = input("Please enter your guess: ")
     if(data == 'q'):
